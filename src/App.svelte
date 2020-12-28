c6e5d88175c9be66567eb31eeed152f007da1f8a
krishna <krishna.nature91@gmail.com>
2020-12-28 23:27:28 +0000

    Added form for poll question

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Header from './components/Header.svelte'
 	import Footer from './components/Footer.svelte'
+	import CreatePollForm from './components/CreatePollForm.svelte'
 	import Tabs from './common/Tabs.svelte'
 
 // tabs
@@ -18,7 +19,7 @@ const tabChange = (e) => {
 	{#if activeItem === 'Current polls'}
 		<p>Poll list components goes here</p>
 	{:else if activeItem === 'Add New Poll'}	
-		<p>New poll from component goes here</p>
+		<CreatePollForm />
 	{/if}
 </main>
 <Footer />

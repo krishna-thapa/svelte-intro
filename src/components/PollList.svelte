137ddad7394cdb33809c22f8126234445aa81f29
krishna <krishna.nature91@gmail.com>
2020-12-31 14:51:03 +0000

    Transition basic is added

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,5 +1,7 @@
 <script>
   // import { onMount, onDestroy } from 'svelte';
+  import { fade, slide, scale } from 'svelte/transition';
+  import { flip } from 'svelte/animate';
   import PollStore from '../stores/PollStore.js'
   import PollDetails from './PollDetails.svelte'
   
@@ -20,11 +22,12 @@
   // });
 
   // Note: Instead of using export and destroy to remove sub, just use $PollStore
+  // svelte/transition should be only be used in html dom not in the custom doms
 </script>
 
 <div class="poll-list">
   {#each $PollStore as poll (poll.id)}
-    <div>
+    <div in:fade out:scale|local animate:flip={{duration: 500}}>
       <PollDetails {poll} />
     </div>
   {/each}
